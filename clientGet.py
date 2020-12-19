8a837fd979147349e4040e0e05ed68e357581ae3
gbhgit <gabrielbandeira@lapisco.ifce.edu.br>
2020-12-19 15:16:30 -0300

    add get client

--- clientGet.py
+++ clientGet.py
@@ -6,21 +6,17 @@ from aiocoap import *
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    """Perform a single PUT request to localhost on the default port, URI
-    "/other/block". The request is sent 2 seconds after initialization.
+    protocol = await Context.create_client_context()
 
-    The payload is bigger than 1kB, and thus sent as several blocks."""
+    request = Message(code=GET, uri='coap://localhost/time')
 
-    context = await Context.create_client_context()
-
-    await asyncio.sleep(2)
-
-    payload = b"The quick brown fox jumps over the lazy dog.\n" * 30
-    request = Message(code=PUT, payload=payload, uri="coap://localhost/sendToProcess")
-
-    response = await context.request(request).response
-
-    print('Result: %s\n%r'%(response.code, response.payload))
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
